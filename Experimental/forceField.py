59ea10192c3123e4f6a024996baae367c525c1d9
ssklykov <ssklykov@yandex.ru>
2020-04-23 16:27:37 +0200

    Simple dipole force field calc 2

--- Experimental/forceField.py
+++ Experimental/forceField.py
@@ -5,20 +5,61 @@ Experiments with creation of attractors / repulsers - something resembling the e
 """
 # %% Imports
 import numpy as np
-
+from scipy.spatial.distance import euclidean
+import matplotlib.pyplot as plt
 
 # %% Class definition
 class dipole():
     """Simulation of electrostatic dipole (only mimicking). """
     coordinatesPlusCharge = np.array([0, 0])
     coordinatesMinusCharge = np.array([1, 1])
+    charge = 10
 
-    def __init__(self, coordinatesPlusCharge, coordinatesMinusCharge):
-        """Coordinates transferred as in dimensionless units! No mm, um... Pixel like. 2D case."""
+    def __init__(self, charge: float, coordinatesPlusCharge, coordinatesMinusCharge):
+        """Coordinates transferred as in dimensionless units! No mm, um... Pixel like. 2D case.
+        Charges - equal for plus and minus """
         self.coordinatesPlusCharge = coordinatesPlusCharge
         self.coordinatesMinusCharge = coordinatesMinusCharge
+        self.charge = charge
         if (len(coordinatesPlusCharge) != 2) or (len(coordinatesMinusCharge) != 2):
             raise("Not 2D Coordinates provided")
+        else:
+            print("The dipole initialized with", charge, "as both charges\n", coordinatesPlusCharge,
+                  "as coordinates of a plus charge\n", coordinatesMinusCharge, "as coordinates of a minus charge")
 
-    def forceFieldCalc():
-        pass
+    def forceFieldCalc(self, x: float, y: float) -> tuple:
+        """Calculation of hypothetic force field between two equal charges - plus and minus.
+        Charge - hypothetic, more suitable as eletrostatic constant * charge, non-physical."""
+        distancePlus = euclidean(self.coordinatesPlusCharge, [x, y])  # Distance between + charge and point in 2D dim
+        plusFieldModule = self.charge*np.exp(-(distancePlus)/4)  # Modulus of field tension (hypothetic)
+        xPlusField = plusFieldModule*(x - self.coordinatesPlusCharge[0])  # Hypothetic repulsive field (plus charge)
+        yPlusField = plusFieldModule*(y - self.coordinatesPlusCharge[1])
+        distanceMinus = euclidean(self.coordinatesMinusCharge, [x, y])  # Distance between - charge and point in 2D dim
+        minusFieldModule = self.charge*np.exp(-(distanceMinus)/4)  # Modulus of field tension
+        xMinusField = minusFieldModule*(self.coordinatesMinusCharge[0] - x)  # Hypothetic attractive field (minus)
+        yMinusField = minusFieldModule*(self.coordinatesMinusCharge[1] - y)
+        xDipoleField = xPlusField + xMinusField  # Sum of plus and minus components of force fields
+        yDipoleField = yPlusField + yMinusField
+        return (xDipoleField, yDipoleField)
+
+    def createFieldMap(self, size: int, nPoints: int):
+        """Shows the map of created by a dipole the force field (can be calculated by using 'forceFieldCalc' method
+        The map itself is a square with sizes [dimension x dimension]."""
+        # step = size/nPoints  # Step for making grid (dimensionless, "pixels")
+        xGrid = np.linspace(0, size, nPoints)
+        # print(xGrid)
+        yGrid = np.linspace(0, size, nPoints)
+        x, y = np.meshgrid(xGrid, yGrid)
+        # print(x)
+        ExGrid, EyGrid = np.zeros(nPoints, dtype='float'), np.zeros(nPoints, dtype='float')
+        Ex, Ey = np.meshgrid(ExGrid, EyGrid)
+        for i in range(nPoints):
+            for j in range(nPoints):
+                xP = x[i][j]
+                yP = y[i][j]
+                (Ex[i][j], Ey[i][j]) = self.forceFieldCalc(xP, yP)
+        fig1 = plt.figure()  # Empty figure
+        plt.streamplot(x, y, Ex, Ey, linewidth=1)  # Creation of map of force field (streamlines of a vector flow)
+        fig2 = plt.figure()
+        plt.quiver(x, y, Ex, Ey)
+        # return (Ex, Ey)
--- Experimental/forceField_Testing.py
+++ Experimental/forceField_Testing.py
@@ -5,9 +5,18 @@ Testing of forceField class capabilities.
 @author: ssklykov
 """
 # %% Imports
-
+from forceField import dipole
 
 # %% Parameters
-
+charge = 100
+xPlusCharge = 20
+yPlusCharge = 20
+xMinusCharge = 60
+yMinusCharge = 60
+size = 100
+nPoints = 100
 
 # %% Demo
+dip1 = dipole(charge, [xPlusCharge, yPlusCharge], [xMinusCharge, yMinusCharge])
+# (Ex, Ey) = dip1.createFieldMap(size, nPoints)
+dip1.createFieldMap(size, nPoints)
