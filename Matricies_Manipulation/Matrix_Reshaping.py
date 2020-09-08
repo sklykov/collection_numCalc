17ad2a94d78b21bd60c7ec66a90c45e88dea9278
ssklykov <ssklykov@yandex.ru>
2020-09-08 17:07:48 +0200

    Small demo of matrix manipulation

---
+++ Matricies_Manipulation/Matrix_Reshaping.py
@@ -0,0 +1,24 @@
+"""
+Matricies editing - reshaping 1D matrix to the 2D with special columns.
+
+author: @ssklykov
+"""
+
+# %% Imports
+import numpy as np
+
+# %% Demo
+coordinates = [100, 200, 50, 400]  # Mix of x and y coordinates
+p = len(coordinates) // 2  # Number of objects with such coordinates in 2D space
+nFrames = 8
+x_coordinates = np.zeros((p, nFrames), dtype=float)
+y_coordinates = np.zeros((p, nFrames), dtype=float)
+(rows, cols) = np.shape(x_coordinates)
+# Special feeling of the initialized before matrix
+for i in range(rows):
+    for j in range(cols):
+        x_coordinates[i, j] = float(coordinates[i*2])
+# Special feeling of the initialized before matrix
+for i in range(rows):
+    for j in range(cols):
+        y_coordinates[i, j] = float(coordinates[i*2+1])
