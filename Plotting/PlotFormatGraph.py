faa22fc7eb7661c172fe54bd53c1bf4032fe5cfc
ssklykov <ssklykov@yandex.ru>
2020-10-27 13:59:06 +0100

    Testing of SSH with slightly refactored code

--- Plotting/PlotFormatGraph.py
+++ Plotting/PlotFormatGraph.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-This script is saved for only keeping and possible reusing it in future.
+Perform simple plotting and convey with comments style of the Numpy.
+
 This script has been created for performing simple plotting using the matplotlib library
 The main purpose is to collect commands for fast and fancy formatting of a produced graph
 WARNING: the autocompletion doesn't work in Spyder IDE that has been used for its creation
@@ -15,33 +16,33 @@ import numpy as np
 stepX = 0.1; maxX = 5.0; minX = -5.0;
 x = np.arange(minX, maxX+stepX, stepX)  # More handful than np.linspace (maybe)
 x = np.around(x, 2)  # To guarantee values consistency (only 2 significant digits in float numbers)
-y = np.power(x,2)  # Calculation of x**2 for guaranting consistency of results (np.array - in and out)
-y = np.around(y,1) # To guarantee values consistency (only 2 significant digits in float numbers)
+y = np.power(x, 2)  # Calculation of x**2 for guaranting consistency of results (np.array - in and out)
+y = np.around(y, 1)  # To guarantee values consistency (only 2 significant digits in float numbers)
 
 # Plotting and graph formatting - using OOP concepts
-wSize = 1.2*(9.0/2.54); hSize = (9.0/2.54) # converting santimeters to inches
-plt.rc('font',family='serif')  # I give up to find it without autocompletion in 'figure' or 'Axes' properties
-fig = plt.figure(figsize=(wSize,hSize))  # fig itself... dpi only setting...
+wSize = 1.2*(9.0/2.54); hSize = (9.0/2.54)  # converting santimeters to inches
+plt.rc('font', family='serif')  # I give up to find it without autocompletion in 'figure' or 'Axes' properties
+fig = plt.figure(figsize=(wSize, hSize))  # fig itself... dpi only setting...
 # axes = fig.add_subplot(1,1,1) # axes class
 axes = plt.axes()  # the alternative way, in comparison to the line above, for adding axes to the current figure window
-axes.plot(x,y,'r-',linewidth=3)  # The main plotting function
-axes.set(xlim=(minX,maxX),ylim=(min(y),max(y)))  # set limits for axes
+axes.plot(x, y, 'r-', linewidth=3)  # The main plotting function
+axes.set(xlim=(minX, maxX), ylim=(min(y), max(y)))  # set limits for axes
 
 # set X axis settings (I gave up to find "OOP" way of setting properties in the Spyder IDE)
-majorXTicks = np.arange(minX,maxX+0.1,1)
-minorXTicks = np.arange(minX+0.5,maxX,0.5)
+majorXTicks = np.arange(minX, maxX+0.1, 1)
+minorXTicks = np.arange(minX+0.5, maxX, 0.5)
 axes.set_xticks(majorXTicks)  # set the major ticks
-axes.set_xticks(minorXTicks,minor=True)
-axes.set_xlabel('x',fontsize=14,fontfamily='Liberation Serif')
+axes.set_xticks(minorXTicks, minor=True)
+axes.set_xlabel('x', fontsize=14, fontfamily='Liberation Serif')
 
 # set Y axis settings
-majorYTicks = np.arange(min(y),max(y)+1.1,2)
+majorYTicks = np.arange(min(y), max(y)+1.1, 2)
 axes.set_yticks(majorYTicks)  # set the major ticks
 # axes.set_yticks(minorYTicks,minor=True)
-axes.set_ylabel('y',fontsize=14,fontfamily='Liberation Serif')
+axes.set_ylabel('y', fontsize=14, fontfamily='Liberation Serif')
 
 # Final preparations
-axes.grid(which='both') # it's enought to specify it once
+axes.grid(which='both')  # it's enought to specify it once
 fig.tight_layout()  # making filling of figure box tight
 
 # Figure saving
@@ -50,5 +51,5 @@ fig.tight_layout()  # making filling of figure box tight
 # fig.savefig("Parabola.eps",dpi=200)  # Somehow for presentation works bad!
 # fig.savefig("Parabola.svg",dpi=200)  # For further using in the Inkscape
 
-#print(dir(axes)) # since autocompletion for "axes" doesn't work, this command print all attributes
+# print(dir(axes))  # since autocompletion for "axes" doesn't work, this command print all attributes
 # print(dir(fig))
--- Plotting/ReadPlotCoordinates.py
+++ Plotting/ReadPlotCoordinates.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Oct 14 2019
 This script is saved for only keeping the gathered info and possible reusing it in future.
+
 This script is created for performing simple reading and plotting using the matplotlib library.
 The main emphasis is on the formatting method of graphs
 The txt file for reading contains two columns of coordinates (int numbers)
@@ -12,15 +12,15 @@ Generally, much information has been collected on the StackOverflow website
 import matplotlib.pyplot as plt
 import numpy as np
 
-path = "XY.txt" # Don't forget to specify actual path to file
-if (path!= ""):
+path = "XY.txt"  # Don't forget to specify actual path to file
+if (path != ""):
     try:
-        #%%  Reading data
+        # %%  Reading data
         x = []; y = []  # maybe redundant initializers
         file = open(path, 'r')
         lines = file.readlines()
         for eachLine in lines:
-            strBuff = eachLine.split(" ") # splitting two columns of numbers
+            strBuff = eachLine.split(" ")  # splitting two columns of numbers
             xBuff = strBuff[0]; yBuff = strBuff[1]
             x.append(int(xBuff))  # put variable in a list with X coordinates
             y.append(int(yBuff))
@@ -28,29 +28,30 @@ if (path!= ""):
         # %% Below many commands have beem written in "Matlab style"
         # They have "OOP" alternative style but for many classes somehow autocompletion doesn't work!
         # %% Figure preformatting
-        wSize = 1.2*(7.0/2.54); hSize = (7.0/2.54) # converting santimeters to inches
-        plt.figure(figsize=(wSize,hSize))  # empty container with specified sizes
-        axes = plt.axes() # get the instance of axes for making minor plots and suppress the poped warning
-        plt.rc('font',family='serif') # setting font type
-        plt.plot(x,y,'ro',markersize=9) # formatting line itself / auto adding axes
+        wSize = 1.2*(7.0/2.54); hSize = (7.0/2.54)  # converting santimeters to inches
+        plt.figure(figsize=(wSize, hSize))  # empty container with specified sizes
+        axes = plt.axes()  # get the instance of axes for making minor plots and suppress the poped warning
+        plt.rc('font', family='serif')  # setting font type
+        plt.plot(x, y, 'ro', markersize = 9)  # formatting line itself / auto adding axes
         xMin = min(x); xMax = max(x); yMin = min(y); yMax = max(y)
-        plt.axis([xMin-1,xMax+1,yMin-2,yMax+2]) # specifiying axis ranges in form xmin xmax
-        plt.xlabel('timepoints',fontsize=14,fontfamily='Liberation Serif')
-        plt.ylabel('measurments',fontsize=14,fontfamily='Liberation Serif')
+        plt.axis([xMin-1, xMax+1, yMin-2, yMax+2])  # specifiying axis ranges in form xmin xmax
+        plt.xlabel('timepoints', fontsize=14, fontfamily='Liberation Serif')
+        plt.ylabel('measurments', fontsize=14, fontfamily='Liberation Serif')
         # separation 'timepoints' to even and odd numbers
-        x1=[]; x2=[]
+        x1 = []
+        x2 = []
         for i in range(len(x)):
             if (i % 2 == 0):
                 x1.append(x[i])
             else:
                 x2.append(x[i])
         # ticks handling
-        plt.xticks(x1,fontsize=10,fontfamily='Liberation Serif') # main xticks
-        axes.set_xticks(x2,minor=True)  # minor ticks
-        plt.yticks(np.arange(yMin,yMax+1,step=4),fontsize=10,fontfamily='Liberation Serif')
-        axes.set_yticks(np.arange(yMin+2,yMax-1,step=2),minor=True)
+        plt.xticks(x1, fontsize=10, fontfamily='Liberation Serif')  # main xticks
+        axes.set_xticks(x2, minor=True)  # minor ticks
+        plt.yticks(np.arange(yMin, yMax+1, step=4), fontsize=10, fontfamily='Liberation Serif')
+        axes.set_yticks(np.arange(yMin+2, yMax-1, step=2), minor=True)
 
-        plt.grid(which='both') # the grid ON
+        plt.grid(which='both')  # the grid ON
         # preparing for figure saving
         plt.tight_layout()  # fill the picture better
         # plt.savefig("SimpleLine.png",dpi=300)
