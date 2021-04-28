196f8e36a3eacb0121cc7e79f8a74eb75d2586a0
ssklykov <ssklykov@yandex.ru>
2021-04-28 16:23:14 +0200

    Mask drawing with all possible origins

--- DIP/fluo_beads_sim/fluorescent_beads.py
+++ DIP/fluo_beads_sim/fluorescent_beads.py
@@ -12,7 +12,35 @@ from u_scene import u_scene
 
 # %% class definition
 class image_beads():
-    pass
+
+    # default values
+    width = 10
+    height = 10
+    possible_img_types = ['uint8', 'uint16', 'float']
+    character_size = 5
+    image_type = 'uint8'
+    bead_types = ["even round", "gaussian round", "uneven round"]
+    bead_img = np.zeros((height, width), dtype= image_type)
+
+    def __init__(self, image_type: str = 'uint8', character_size: int = 5, bead_type: str = "even round"):
+        if image_type in self.possible_img_types:
+            self.image_type = image_type
+        else:
+            self.image_type = 'uint8'
+            print("Image type isn't recognized, initialized default 8bit gray image")
+        if bead_type in self.bead_types:
+            self.bead_type = image_type
+        else:
+            self.image_type = 'uint8'
+            print("Image type isn't recognized, initialized default 8bit gray image")
+        if (character_size != 5) or (image_type != 'uint8'):
+            width = character_size*2
+            height = character_size*2
+            self.scene_image = np.zeros((height, width), dtype=self.image_type)
+            if image_type == 'uint16':
+                self.maxPixelValue = 65535
+            else:
+                self.maxPixelValue = 1.0  # According to the specification of scikit-image
 
 
 # %% General parameters
@@ -20,5 +48,5 @@ width = 1000
 height = 1000
 
 # %% Testing features
-pic = u_scene(width, height, 'uint8')
-
+if __name__ == '__main__':
+    pic = u_scene(width, height, 'uint8')
--- DIP/fluo_beads_sim/u_scene.py
+++ DIP/fluo_beads_sim/u_scene.py
@@ -12,16 +12,36 @@ import matplotlib.pyplot as plt
 
 # %% class definition
 class u_scene():
+    """Class composing all capabilities of building image (numpy 2D array) with some objects drawn on the scene.
+    The image commonly is designated as width x height (e.g., 800x600)"""
 
     # default values
     width = 100
     height = 100
     possible_img_types = ['uint8', 'uint16', 'float']
     image_type = 'uint8'
-    scene_image = np.zeros((width, height), dtype=image_type)
+    scene_image = np.zeros((height, width), dtype=image_type)
     maxPixelValue = 255
 
     def __init__(self, width: int, height: int, image_type: str = 'uint8'):
+        """
+        Initialize the blank (dark) scene image with the specified type (800x600 8bit image as an example)
+
+        Parameters
+        ----------
+        width : int
+            Width of the initialized image (scene)
+        height : int
+            Height of the initialized image (scene)
+        image_type : str, optional
+            Image type used for pixel value calculations. Possible values are: 'uint8', 'uint16', 'float'.
+            The default is 'uint8'.
+
+        Returns
+        -------
+        None.
+
+        """
         width = abs(width)
         height = abs(height)
         if width > 0:
@@ -34,92 +54,153 @@ class u_scene():
             self.image_type = 'uint8'
             print("Image type isn't recognized, initialized default 8bit gray image")
         if (width != 100) or (height != 100) or (image_type != 'uint8'):
-            self.scene_image = np.zeros((width, height), dtype=self.image_type)
+            self.scene_image = np.zeros((height, width), dtype=self.image_type)
             if image_type == 'uint16':
                 self.maxPixelValue = 65535
             else:
-                self.maxPixelValue = 1.0  # According to the specification in scikit-image
+                self.maxPixelValue = 1.0  # According to the specification of scikit-image
 
-    def add_mask(self, x_start: int, y_start: int, mask):
+    def cast_pixels_sum(self, pixels_sum):
+        """
+        Casting of input result of pixel summing to conform with data type of the used image.
+
+        Parameters
+        ----------
+        pixels_sum : TYPE
+            Sum of pixels (mask + scene (background)).
+
+        Returns
+        -------
+        value_returned : uint8, uint16 or float
+            Returns casted / corrected pixel value.
+
+        """
+        if (pixels_sum) <= self.maxPixelValue:
+            # additional conversion for insuring of conformity with data type
+            if self.image_type == 'uint8':
+                value_returned = np.uint8(pixels_sum)
+            elif self.image_type == 'uint16':
+                value_returned = np.uint16(pixels_sum)
+            else:
+                value_returned = np.float64(pixels_sum)
+        else:
+            value_returned = self.maxPixelValue
+        return value_returned
+
+    def get_j_finish(self, j_start: int, nCols: int) -> int:
+        if ((j_start + nCols) < self.width):  # checking that starting/ending of summing are not out of bounds
+            j_finish = j_start + nCols
+        else:
+            j_finish = self.width
+        return j_finish
+
+    def get_i_finish(self, i_start: int, nRows: int) -> int:
+        if ((i_start + nRows) < self.height):  # checking that starting/ending of summing are not out of bounds
+            i_finish = i_start + nRows
+        else:
+            i_finish = self.height
+        return i_finish
+
+    def add_mask(self, i_start: int, j_start: int, mask):
+        """
+        Adding the "mask" - representation of the object (basically, less than the scene (background) image).
+        Contradictory, i_start represents "y" coordinate, j_start - "x", due to array representation of column and row
+
+        Parameters
+        ----------
+        i_start : int
+            Start pixel (y coordinate) for drawing of an image ("mask").
+        j_start : int
+            Start pixel (x coordinate) for drawing of an image ("mask").
+        mask : TYPE
+            2D np.array ("mask") with pixel values which representing the object.
+
+        Returns
+        -------
+        None.
+
+        """
         (nRows, nCols) = np.shape(mask)  # getting of sizes of mask
-        # print(nRows, nCols, " - sizes of input mask")  # Checking the proper working
 
         # Below is checking that the mask is not empty, it should be 1x1 matrix at least
         if (nRows == 0) or (nCols == 0):
             raise(IndexError('Provided mask is empty along some of its axis'))
 
-        # Below is checking that the x_start and y_start makes sense to apply to the scene image:
-        # x_start and y_start could be negative, but at least 1 point should be added to a scene
-        if ((x_start + nRows) < 1) or ((y_start + nCols) < 1):
-            raise(IndexError('Provided x_start or y_start is not conformed with mask sizes'))
+        # Below is checking that the i_start and j_start makes sense to apply to the scene image:
+        # i_start and j_start could be negative, but at least 1 point should be added to a scene
+        # also, j associates with WIDTH, so with # of columns! i - with rows!
+        if ((i_start + nRows) < 1) or ((j_start + nCols) < 1):
+            raise(IndexError('Provided i_start or j_start is not conformed with the mask sizes'))
 
-        # Below is checking filling parameters (x_start, y_start) is laying on an scene image
-        if (x_start >= self.height) or (y_start >= self.width):
+        # Below is checking filling parameters (i_start, j_start) is laying on an scene image
+        if (i_start >= self.height) or (j_start >= self.width):
             raise(IndexError("Starting indices for adding mask is out of scene image bounds"))
 
-        # TODO
-        # 1) x_start, y_start - could be negative, make the logic for it - fiiling out of bounds of an image
-
-        # x_start, y_start > 0 both, filling some mask into a scene image
-        if (x_start >= 0) and (y_start >= 0):
+        # i_start, j_start > 0 both, filling some mask into a scene image - basic check for conformity
+        if (i_start >= 0) and (j_start >= 0) and (nRows > 0) and (nCols > 0):
             # Attempt to speed up the adding mask to a scene: transferring pixel values as chunk with rows
-            if ((x_start + nRows) < self.height):  # checking that fast sum over x axis could be performed
-                x_finish = x_start + nRows
-                if ((y_start + nCols) < self.width):  # checking that starting and ending of summing are not of bounds
-                    y_finish = y_start + nCols
-                else:
-                    y_finish = self.width
-                for j in range(y_start, y_finish):  # summing along y axis
-                    # checking the conformity with image type
-                    if np.max(self.scene_image[x_start:x_finish, j] + mask[:, j-y_start]) <= self.maxPixelValue:
-                        self.scene_image[x_start:x_finish, j] += mask[:, j-y_start]  # fast adding mask to a scene
-                    else:
-                        # checking each pixel from a scene and added from a mask pixel to be in range with image type
-                        for i in range(x_start, x_finish):
-                            pixels_sum = self.scene_image[i, j] + mask[i-x_start, j-y_start]
-                            if (pixels_sum) <= self.maxPixelValue:
-                                if self.image_type == 'uint8':
-                                    self.scene_image[i, j] = np.uint8(pixels_sum)  # additional conversion
-                            else:
-                                self.scene_image[i, j] = self.maxPixelValue
+            if ((i_start + nRows) < self.height):  # checking that fast sum over y axis could be performed
+                i_finish = i_start + nRows
+                j_finish = self.get_j_finish(j_start, nCols)
 
-            # Attempt to speed up the adding mask to a scene: transferring pixel values as chunk with columns
-            elif ((y_start + nCols) < self.width):  # checking that fast sum over x axis could be performed
-                y_finish = y_start + nCols
-                if ((x_start + nRows) < self.height):  # checking that starting and ending of summing are not of bounds
-                    x_finish = x_start + nRows
-                else:
-                    x_finish = self.height
-                for i in range(x_start, x_finish):  # summing along y axis
+                # "Fast summing" - adding the whole rows (all columns) to the image (scene)
+                for j in range(j_start, j_finish):  # summing along j axis
                     # checking the conformity with image type
-                    if np.max(self.scene_image[i, y_start:y_finish] + mask[i-x_start, :]) <= self.maxPixelValue:
-                        self.scene_image[i, y_start:y_finish] += mask[i-x_start, :]  # fast adding mask to a scene
+                    if np.max(self.scene_image[i_start:i_finish, j] + mask[:, j-j_start]) <= self.maxPixelValue:
+                        self.scene_image[i_start:i_finish, j] += mask[:, j-j_start]  # fast adding mask to a scene
                     else:
                         # checking each pixel from a scene and added from a mask pixel to be in range with image type
-                        for j in range(y_start, y_finish):
-                            pixels_sum = self.scene_image[i, j] + mask[i-x_start, j-y_start]
-                            if (pixels_sum) <= self.maxPixelValue:
-                                if self.image_type == 'uint8':
-                                    self.scene_image[i, j] = np.uint8(pixels_sum)  # additional conversion
-                            else:
-                                self.scene_image[i, j] = self.maxPixelValue
+                        for i in range(i_start, i_finish):
+                            pixels_sum = self.scene_image[i, j] + mask[i-i_start, j-j_start]
+                            self.scene_image[i, j] = self.cast_pixels_sum(pixels_sum)
+
+            # Attempt to speed up the adding mask to a scene: transferring pixel values as a chunk with columns
+            elif ((j_start + nCols) < self.width):  # checking that fast sum over i axis could be performed
+                j_finish = j_start + nCols
+                i_finish = self.get_i_finish(i_start, nRows)
+
+                # "Fast summing" - along column - adding all rows at once
+                for i in range(i_start, i_finish):  # summing along j axis
+                    # checking the conformity with image type
+                    if np.max(self.scene_image[i, j_start:j_finish] + mask[i-i_start, :]) <= self.maxPixelValue:
+                        self.scene_image[i, j_start:j_finish] += mask[i-i_start, :]  # fast adding mask to a scene
+                    else:
+                        # checking each pixel from a scene and added from a mask pixel to be in range with image type
+                        for j in range(j_start, j_finish):
+                            pixels_sum = self.scene_image[i, j] + mask[i-i_start, j-j_start]
+                            self.scene_image[i, j] = self.cast_pixels_sum(pixels_sum)
 
             # filling right upper corner with exceptional case - when mask is out of image bounds
             else:
-                x_finish = self.height
-                y_finish = self.width
-                for i in range(x_start, x_finish):
-                    for j in range(y_start, y_finish):
-                        pixels_sum = self.scene_image[i, j] + mask[i-x_start, j-y_start]
-                        if (pixels_sum) <= self.maxPixelValue:
-                            if self.image_type == 'uint8':
-                                self.scene_image[i, j] = np.uint8(pixels_sum)  # additional conversion
-                        else:
-                            self.scene_image[i, j] = self.maxPixelValue
+                i_finish = self.height
+                j_finish = self.width
+                for i in range(i_start, i_finish):
+                    for j in range(j_start, j_finish):
+                        pixels_sum = self.scene_image[i, j] + mask[i-i_start, j-j_start]
+                        self.scene_image[i, j] = self.cast_pixels_sum(pixels_sum)
+
+        # Making correction of i_start, j_start if some of them is negative for working with partial mask overlap
+        if (i_start < 0) or (j_start < 0):
+            i_mask_start = 0
+            j_mask_start = 0
+            if (i_start < 0):
+                nRows += i_start  # it will draw the mask if it partially overlaps with image boundaries
+                i_mask_start = abs(i_start)
+                i_start = 0
+            if (j_start < 0):
+                nCols += j_start
+                j_mask_start = abs(j_start)
+                j_start = 0
+            i_finish = self.get_i_finish(i_start, nRows)
+            j_finish = self.get_j_finish(j_start, nCols)
+            for i in range(i_start, i_finish):
+                for j in range(j_start, j_finish):
+                    pixels_sum = self.scene_image[i, j] + mask[i+i_mask_start, j+j_mask_start]
+                    self.scene_image[i, j] = self.cast_pixels_sum(pixels_sum)
 
     def plot_image(self):
         """
-        Plotting the self.scene composed with added masks / noise.
+        Plotting the self.scene composed with added masks (objects) / noise.
 
         Returns
         -------
@@ -132,14 +213,14 @@ class u_scene():
         # origin - for adjusting origin of pixels (0, 0), extent - regulation of axis values
         # extent = (-0.5, numcols-0.5, -0.5, numrows-0.5)) - for origin = 'lower' - documents
         plt.imshow(self.scene_image, cmap=plt.cm.gray, aspect='auto', origin='lower',
-                   extent=(0, self.height, 0, self.width))
+                   extent=(0, self.width, 0, self.height))
         plt.tight_layout()
 
 
 # %% Testing class methods / construction
 if __name__ == '__main__':
     uScene = u_scene(100, 100, 'uint8')
-    mask = np.ones((10, 10), dtype='uint8')
+    mask = np.ones((20, 20), dtype='uint8')
     mask = mask[:, :]*256
-    uScene.add_mask(0, 0, mask)
+    uScene.add_mask(40, 40, mask)
     uScene.plot_image()
