16359ed5d6a2b976fe4de6a9ace258a68bb13af1
Sergei <ssklykov@yandex.ru>
2022-02-17 12:24:34 +0100

    Calculation of integrals on Zernike pol-s debugged

--- ZernikePolynomials/calc_zernikes_sh_wfs.py
+++ ZernikePolynomials/calc_zernikes_sh_wfs.py
@@ -107,6 +107,34 @@ def get_localCoM_matrix(image: np.ndarray, min_dist_peaks: int = 15, threshold_a
 def get_integr_limits_circular_lenses(image: np.ndarray, aperture_radius: float = 15.0,
                                       min_dist_peaks: int = 15, threshold_abs: float = 2,
                                       unknown_geometry: bool = True, debug: bool = False):
+    """
+    Calculate integration limits on theta for circular lenses (sub-apertures) and polar coordinates of their centers.
+
+    Parameters
+    ----------
+    image : np.ndarray
+        The image acquired by a Shack-Hartmann sensor.
+    aperture_radius : float, optional
+        In pixels on the image. The default is 15.0.
+    min_dist_peaks : int, optional
+        Minimum distance of peaks on the image in pixels. The default is 15.
+    threshold_abs : float, optional
+        Absolute threshold intensity value as the starting value for peak defining. The default is 2.
+    unknown_geometry : bool, optional
+        If the actual sizes of subapertures and their centers relative to the image are unknown. The default is True.
+    debug : bool, optional
+        For plotting some of the calculated values (sub-apertures, angels, etc.). The default is False.
+
+    Returns
+    -------
+    integration_limits : ndarray of floats with size (N sub-apertures, 2)
+        Integration limits on theta.
+    theta0 : ndarray of floats (N sub-apertures, 1)
+        Angles theta of sub-aperture centers from the global polar coordinates on the frame (image).
+    rho0 : ndarray of floats (N sub-apertures, 1)
+        Radiuses r of sub-aperture centers from the global polar coordinates on the frame (image).
+
+    """
     # Plotting the estimated circular apertures with the centers the same as found on the image local peaks
     # This is only for the unknown geometry applied
     if unknown_geometry and debug:
@@ -131,7 +159,7 @@ def get_integr_limits_circular_lenses(image: np.ndarray, aperture_radius: float 
             rho0[i] = np.sqrt(np.power(x_relative, 2) + np.power(y_relative, 2))  # radial coordinate of lens pupil
             # Below - manual recalculation of angles
             # !!!: axes X, Y => cols, rows on an image, center of polar coordinates - center of an image
-            # !!!: angles are calculated relative to swapped X and Y axes, calculation below in grads
+            # !!!: angles are calculated relative to swapped X and Y axes, calculation below in grads, in the translated to radians
             if (x_relative > 0.0) and (y_relative > 0.0):
                 theta0[i] = np.arctan(x_relative/y_relative)*(180/np.pi)
             elif (y_relative < 0.0) and (x_relative > 0.0):
@@ -151,22 +179,69 @@ def get_integr_limits_circular_lenses(image: np.ndarray, aperture_radius: float 
             theta_a[i] = np.round(theta0[i] - np.arctan(aperture_radius/rho0[i])*(180/np.pi), 3)
             theta_b[i] = np.round(theta0[i] + np.arctan(aperture_radius/rho0[i])*(180/np.pi), 3)
             integration_limits[i] = (theta_a[i], theta_b[i])
+        # Plotting the calculated coordinates in polar projection for checking that all sub-aperture centers defined correctly
         plt.figure()
         plt.axes(projection='polar')
         plt.plot(np.radians(theta0), rho0, '.')
         plt.tight_layout()
+        theta0 = np.radians(theta0)
+        integration_limits = np.radians(integration_limits)
         return (integration_limits, theta0, rho0)
 
 
-def rho_ab(rho0: float, theta: float, theta0: float, aperture_radius: float) -> float:
+def rho_ab(rho0: float, theta: float, theta0: float, aperture_radius: float) -> tuple:
+    """
+    Calculate of integration limits on sub-aperture (crossed by global radius from the polar coordinates).
+
+    For the drawing - check mentioned dissertation.
+
+    Parameters
+    ----------
+    rho0 : float
+        R coordinate of the sub-aperture center (O).
+    theta : float
+        Current angle from the global polar coordinate (relative to the frame's center).
+    theta0 : float
+        Theta coordinate of the sub-aperture center (O).
+    aperture_radius : float
+        Radius of a sub-aperture.
+
+    Returns
+    -------
+    tuple
+        As (rho_a, rho_b) - the integration limits for further integration of Zernike polynomials.
+
+    """
     cosin = np.cos(theta-theta0)
-    cosin2 = np.cos((theta-theta0)*(theta-theta0))
-    rho_a = rho0*cosin - np.sqrt(rho0*(-cosin2 + 1) + aperture_radius)  # ??? BUG
-    rho_b = rho0*cosin + np.sqrt(rho0*(-cosin2 + 1) + aperture_radius)  # ???
+    cosinsq = np.power(np.cos(theta-theta0), 2)
+    rho0sq = rho0*rho0
+    apertureRsq = aperture_radius*aperture_radius
+    rho_a = rho0*cosin - np.sqrt(rho0sq*(cosinsq - 1) + apertureRsq)
+    rho_b = rho0*cosin + np.sqrt(rho0sq*(cosinsq - 1) + apertureRsq)
     return (rho_a, rho_b)
 
 
 def rho_integral_funcX(r: float, theta: float, m: int, n: int) -> float:
+    """
+    Return function under the integration on X axis specified in the thesis.
+
+    Parameters
+    ----------
+    r : float
+        Polar coordinate (rho).
+    theta : float
+        Polar coordinate (theta).
+    m : int
+        Azimutal order of the Zernike polynomial.
+    n : int
+        Radial order of the Zernike polynomial.
+
+    Returns
+    -------
+    float
+        Function value.
+
+    """
     derivRmn = radial_polynomial_derivative_dr(m, n, r)
     Rmn = radial_polynomial(m, n, r)
     Angularmn = triangular_function(m, theta)
@@ -175,6 +250,26 @@ def rho_integral_funcX(r: float, theta: float, m: int, n: int) -> float:
 
 
 def rho_integral_funcY(r: float, theta: float, m: int, n: int) -> float:
+    """
+    Return function under the integration on Y axis specified in the thesis.
+
+    Parameters
+    ----------
+    r : float
+        Polar coordinate (rho).
+    theta : float
+        Polar coordinate (theta).
+    m : int
+        Azimutal order of the Zernike polynomial.
+    n : int
+        Radial order of the Zernike polynomial.
+
+    Returns
+    -------
+    float
+        Function value.
+
+    """
     derivRmn = radial_polynomial_derivative_dr(m, n, r)
     Rmn = radial_polynomial(m, n, r)
     Angularmn = triangular_function(m, theta)
@@ -183,17 +278,41 @@ def rho_integral_funcY(r: float, theta: float, m: int, n: int) -> float:
 
 
 def calc_integrals_on_apertures(integration_limits: list, theta0: np.ndarray, rho0: np.ndarray, m: int, n: int,
-                                aperture_radius: float = 15.0, n_steps: int = 20):
+                                aperture_radius: float = 15.0, n_steps: int = 50):
+    """
+    Calculate integrals using trapezodial rule specified in the mentioned thesis on X and Y axes for the specified Zernike polynomial.
 
+    Parameters
+    ----------
+    integration_limits : list
+        Calculated previously on theta polar coordinate.
+    theta0 : np.ndarray
+        Polar coordinates theta of sub-aperture centers.
+    rho0 : np.ndarray
+        Polar coordinates r of sub-aperture centers.
+    m : int
+        Azimutal order of the Zernike polynomial.
+    n : int
+        Radial order of the Zernike polynomial..
+    aperture_radius : float, optional
+        Radius of sub-aperture in pixels on the image. The default is 15.0.
+    n_steps : int, optional
+        Number of integration steps for both integrals. The default is 50.
+
+    Returns
+    -------
+    integral_values : ndarray with sizes (Number of sub-apertures, 2)
+        Resulting integration values for each sub-aperture and for both X and Y axes.
+
+    """
     # Integration on theta and rho - for X, Y integrals of Zernike polynonial made on the sub-aperture
     integral_values = np.zeros((len(integration_limits), 2), dtype='float')  # Doubled values - for X,Y axes
     calibration = 1/(np.pi*aperture_radius*aperture_radius)
-
+    # For each sub-aperture below integration on (r, theta) of the Zernike polynomials
     for i_subaperture in range(len(integration_limits)):
-
-        (theta_a, theta_b) = integration_limits[i_subaperture]  # Calculated previously integration steps on theta
+        (theta_a, theta_b) = integration_limits[i_subaperture]  # Calculated previously integration steps on theta (radians)
         delta_theta = (theta_b - theta_a)/n_steps  # Step for integration on theta
-        theta = theta_a  # initial value
+        theta = theta_a  # initial value of theta
         # Integration over theta (trapezoidal rule)
         integral_sumX = 0.0; integral_sumY = 0.0
         for j_theta in range(n_steps+1):
@@ -212,18 +331,18 @@ def calc_integrals_on_apertures(integration_limits: list, theta0: np.ndarray, rh
                     integral_sum_rhoX += rho_integral_funcX(rho, theta, m, n)
                     integral_sum_rhoY += rho_integral_funcY(rho, theta, m, n)
                 rho += delta_rho
-            integral_sum_rhoX *= delta_rho; integral_sum_rhoY *= delta_rho
+
+            integral_sum_rhoX *= delta_rho; integral_sum_rhoY *= delta_rho  # End of integration on rho(r)
 
             if (j_theta == 0) and (j_theta == n_steps):
                 integral_sumX += 0.5*integral_sum_rhoX; integral_sumY += 0.5*integral_sum_rhoY
             else:
                 integral_sumX += integral_sum_rhoX; integral_sumY += integral_sum_rhoY
             theta += delta_theta
+        integral_sumX *= delta_theta; integral_sumY *= delta_theta  # End of integration on theta
 
-        integral_sumX *= delta_theta; integral_sumY *= delta_theta
-
-        integral_values[i_subaperture, 0] = calibration*integral_sumX
-        integral_values[i_subaperture, 1] = calibration*integral_sumY
+        integral_values[i_subaperture, 0] = calibration*integral_sumX  # Partially calibration on the area of sub-aperture
+        integral_values[i_subaperture, 1] = calibration*integral_sumY  # Partially calibration on the area of sub-aperture
     return integral_values
 
 
@@ -239,15 +358,17 @@ diff_nonaberrated = (nonaberrated - background); diff_nonaberrated = img_as_ubyt
 diff_aberrated = (aberrated - background); diff_aberrated = img_as_ubyte(diff_aberrated)
 # plt.figure(); plt.imshow(diff_nonaberrated); plt.figure(); plt.imshow(diff_aberrated)
 
-# %% Shifts between aberrated and non-aberrated pictures calculation
+# %% Calculation of shifts between non- and aberrated images, integrals of Zernike polynomials on sub-apertures
 get_localCoM_matrix(diff_nonaberrated, plot=True)  # Plotting of results on an image for debugging
-t1 = time.time()
-coms_nonaberrated = get_localCoM_matrix(diff_nonaberrated)
+t1 = time.time(); coms_nonaberrated = get_localCoM_matrix(diff_nonaberrated)
 coms_aberrated = get_localCoM_matrix(diff_aberrated)
 coms_nonaberrated = np.sort(coms_nonaberrated, axis=0); coms_aberrated = np.sort(coms_aberrated, axis=0)
-diff_coms = coms_nonaberrated - coms_aberrated
-t2 = time.time()
+diff_coms = coms_nonaberrated - coms_aberrated; t2 = time.time()
 print("Shift of local center of masses between non- and aberrated images takes: ", np.round(t2-t1, 3), "s")
+t1 = time.time(); m = -1; n = 1
 (integration_limits, theta0, rho0) = get_integr_limits_circular_lenses(diff_nonaberrated, debug=True)
-theta0 = np.radians(theta0)
-integral_values = calc_integrals_on_apertures(integration_limits, theta0, rho0, -1, 1)
+integral_values = calc_integrals_on_apertures(integration_limits, theta0, rho0, m, n)
+t2 = time.time()
+print(f"Integration of the single Zernike polynomial ({m},{n}) takes:", np.round(t2-t1, 3), "s")
+
+# %% Testing of the decomposition of aberrations into the sum of Zernike polynomials
