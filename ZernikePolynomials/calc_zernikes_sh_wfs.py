Fixed calculation of shifts - with signs now

ZernikePolynomials/calc_zernikes_sh_wfs.py
@@ -651,16 +651,21 @@ def get_overall_coms_shifts(pics_folder: str = "pics", background_pic_name: str 
         # Below - the algorithm for searching for the right pair between CoMs on non - and aberrated image
         for i in range(np.size(coms_aberrated, 0)):
             j = 0
+            # For finding real shift - only matching pair with minimum of shift between coordinates;
+            # But recorded shift should be with the sign!!!
             diffX = abs(coms_aberrated[i, 0] - coms_nonaberrated[j, 0]); diffY = abs(coms_aberrated[i, 1] - coms_nonaberrated[j, 1])
+            diffX_sign = (coms_aberrated[i, 0] - coms_nonaberrated[j, 0]); diffY_sign = (coms_aberrated[i, 1] - coms_nonaberrated[j, 1])
             if (diffX < (min_dist_peaks - 1) and diffY < (min_dist_peaks - 1)):
-                coms_shifts[i, 0] = diffX; coms_shifts[i, 1] = diffY
+                coms_shifts[i, 0] = diffX_sign; coms_shifts[i, 1] = diffY_sign
             else:
                 # Searching for the appropriate candidate (actual shift should lay in the ranges on both axes) from nonaberrated CoMs
                 for j in range(1, np.size(coms_nonaberrated, 0)):
                     diffX = abs(coms_aberrated[i, 0] - coms_nonaberrated[j, 0])
+                    diffX_sign = (coms_aberrated[i, 0] - coms_nonaberrated[j, 0])
                     diffY = abs(coms_aberrated[i, 1] - coms_nonaberrated[j, 1])
+                    diffY_sign = (coms_aberrated[i, 1] - coms_nonaberrated[j, 1])
                     if (diffX < (min_dist_peaks - 1) and diffY < (min_dist_peaks - 1)):
-                        coms_shifts[i, 0] = diffX; coms_shifts[i, 1] = diffY; break  # Stop if the candidate found
+                        coms_shifts[i, 0] = diffX_sign; coms_shifts[i, 1] = diffY_sign; break  # Stop if the candidate found
     else:
         # If the number of detected peaks is different for aberrated and non-aberrated pictures
         if np.size(coms_nonaberrated, 0) > np.size(coms_aberrated, 0):
@@ -671,15 +676,19 @@ def get_overall_coms_shifts(pics_folder: str = "pics", background_pic_name: str 
             for i in range(np.size(coms_aberrated, 0)):
                 j = 0
                 diffX = abs(coms_aberrated[i, 0] - coms_nonaberrated[j, 0]); diffY = abs(coms_aberrated[i, 1] - coms_nonaberrated[j, 1])
+                diffX_sign = (coms_aberrated[i, 0] - coms_nonaberrated[j, 0])
+                diffY_sign = (coms_aberrated[i, 1] - coms_nonaberrated[j, 1])
                 if (diffX < (min_dist_peaks - 1) and diffY < (min_dist_peaks - 1)):
-                    coms_shifts[i, 0] = diffX; coms_shifts[i, 1] = diffY
+                    coms_shifts[i, 0] = diffX_sign; coms_shifts[i, 1] = diffY_sign
                 else:
-                    # Searching for the appropriate candidate (actual shift should lay in the ranges on both axes) from nonaberrated CoMs
+                    # Searching for the candidate (actual shift should lay in the ranges on both axes) from nonaberrated CoMs
                     for j in range(1, np.size(coms_nonaberrated, 0)):
                         diffX = abs(coms_aberrated[i, 0] - coms_nonaberrated[j, 0])
+                        diffX_sign = (coms_aberrated[i, 0] - coms_nonaberrated[j, 0])
                         diffY = abs(coms_aberrated[i, 1] - coms_nonaberrated[j, 1])
+                        diffY_sign = (coms_aberrated[i, 1] - coms_nonaberrated[j, 1])
                         if (diffX < (min_dist_peaks - 1) and diffY < (min_dist_peaks - 1)):
-                            coms_shifts[i, 0] = diffX; coms_shifts[i, 1] = diffY; break  # Stop if the candidate found
+                            coms_shifts[i, 0] = diffX_sign; coms_shifts[i, 1] = diffY_sign; break  # Stop if the candidate found
         else:
             coms_shifts = np.zeros((np.size(coms_nonaberrated, 0), 2), dtype='float')
             pic_integral_limits = diff_nonaberrated; coms_integral_limits = coms_nonaberrated

ZernikePolynomials/calibrations/calibrations_tests.py
@@ -41,11 +41,13 @@ print("60 -> 80 steps: ", np.max(diff_Z80_Z60))
 # print("10 -> 20 steps: ", np.max(diff_Z20_Z10))
 
 # %% Compose already calculated values
-Z1_name = "ZSHwfs[(-1, 1), (1, 1), (-2, 2), (0, 2), (2, 2)].npy"; Z1_path = os.path.join(calibration_folder, Z1_name)
-Z2_name = "ZSHwfs[(-3, 3), (-1, 3), (1, 3), (3, 3)].npy"; Z2_path = os.path.join(calibration_folder, Z2_name)
-Z3_name = "ZSHwfs[(-4, 4), (-2, 4), (0, 4), (2, 4), (4, 4)].npy"; Z3_path = os.path.join(calibration_folder, Z3_name)
-Z1 = np.load(Z1_path); Z2 = np.load(Z2_path); Z3 = np.load(Z3_path)
+Z1_name = "ZSHwfs[(-1, 1), (1, 1)].npy"; Z1_path = os.path.join(calibration_folder, Z1_name)
+Z2_name = "ZSHwfs[(-2, 2), (0, 2), (2, 2)].npy"; Z2_path = os.path.join(calibration_folder, Z2_name)
+Z3_name = "ZSHwfs[(-3, 3), (-1, 3), (1, 3), (3, 3)].npy"; Z3_path = os.path.join(calibration_folder, Z3_name)
+Z4_name = "ZSHwfs[(-4, 4), (-2, 4), (0, 4), (2, 4), (4, 4)].npy"; Z4_path = os.path.join(calibration_folder, Z4_name)
+Z1 = np.load(Z1_path); Z2 = np.load(Z2_path); Z3 = np.load(Z3_path); Z4 = np.load(Z4_path)
+diffZ2col = Z2[:, 0] + Z2[:, 5]; diffZ2col2 = Z2[:, 1] - Z2[:, 4]
 Z_overall_name = "Calibration14ZernikesShH.npy"; Z_overall_path = os.path.join(calibration_folder, Z_overall_name)
 if not(os.path.exists(Z_overall_path)):
-    Z_overall = np.concatenate((Z1, Z2, Z3), axis=1)
+    Z_overall = np.concatenate((Z1, Z2, Z3, Z4), axis=1)
     np.save(Z_overall_path, Z_overall)

ZernikePolynomials/reconstruction_test.py
@@ -16,8 +16,8 @@ import numpy as np
 shwfs = True; repo_pics = False
 
 # %% Making calibration (integration of Zernike polynomials over sub-apertures) once and reading the integral matrix later
-zernikes_set = [(-1, 1)]
-# zernikes_set = [(-1, 1), (1, 1)]
+# zernikes_set = [(-1, 1)]
+zernikes_set = [(-1, 1), (1, 1)]
 # zernikes_set = [(-2, 2), (0, 2), (2, 2)]
 # zernikes_set = [(-3, 3), (-1, 3), (1, 3), (3, 3)]
 # zernikes_set = [(-4, 4), (-2, 4), (0, 4), (2, 4), (4, 4)]
@@ -26,8 +26,8 @@ zernikes_set = [(-1, 1)]
 # %% Tests on the recorded pictures from the Shack-Hartmann sensor
 # Manual change the working directory to the folder with stored pictures, outside the repository
 if shwfs:
-    os.chdir(".."); os.chdir(".."); os.chdir("sh_wfs"); aberrated_pic_name = "DefocusPic0.png"
-    plot = False; debug = True; aperture_radius = 11.0
+    os.chdir(".."); os.chdir(".."); os.chdir("sh_wfs"); aberrated_pic_name = "DefocusPic2.png"
+    plot = False; debug = False; aperture_radius = 11.0
     (coms_shifts, coms_integral_lim, pic_int_lim) = get_overall_coms_shifts(pics_folder=os.getcwd(),
                                                                             background_pic_name="backgroundPic2.png",
                                                                             nonaberrated_pic_name="nonAberrationPic2.png",
@@ -40,7 +40,7 @@ if shwfs:
     current_path = os.path.dirname(__file__)  # get path to the folder containing the script
     calibrations = os.path.join(current_path, "calibrations")
     calibration_path = os.path.join(calibrations, calibration_file_name)
-    precalculated_zernikes = os.path.join(calibrations, "Calibration12ZernikesShH_wtTilts.npy")
+    precalculated_zernikes = os.path.join(calibrations, "Calibration14ZernikesShH.npy")
     if not(os.path.exists(precalculated_zernikes)):
         if not(os.path.exists(calibration_path)):
             t1 = time.time()
@@ -58,7 +58,7 @@ if shwfs:
                          (1, 3), (3, 3), (-4, 4), (-2, 4), (0, 4), (2, 4), (4, 4)]
         for i in range(len(set14zernikes)):
             print(get_classical_polynomial_name(set14zernikes[i]), ":", np.round(alpha_coefficients[i], 4))
-        plot_zps_polar(zernikes_set, title="reconstraction of " + aberrated_pic_name,
+        plot_zps_polar(set14zernikes, title="reconstraction of " + aberrated_pic_name,
                        tuned=True, alpha_coefficients=alpha_coefficients)
 
 # %% Calibration of pictures shared in the repository
@@ -90,5 +90,5 @@ if repo_pics:
                          (1, 3), (3, 3), (-4, 4), (-2, 4), (0, 4), (2, 4), (4, 4)]
         for i in range(len(set14zernikes)):
             print(get_classical_polynomial_name(set14zernikes[i]), ":", np.round(alpha_coefficients[i], 4))
-        plot_zps_polar(zernikes_set, title="reconstraction of pics from the open repository",
+        plot_zps_polar(set14zernikes, title="reconstraction of pics from the open repository",
                        tuned=True, alpha_coefficients=alpha_coefficients)

ZernikePolynomials/zernike_pol_calc.py
@@ -399,6 +399,9 @@ def test():
     m = 1; n = 5
     assert radial_polynomial(m, n, r) == 10*np.power(r, 5) - 12*r*r*r + 3*r, f'Implemented R{m, n} not equal to tabulated Zernike'
     assert radial_polynomial_derivative_dr(m, n, r) == 50*np.power(r, 4) - 36*r*r + 3, f'Implemented dR{m, n} != to the derivative'
+    m = 4; n = 6
+    assert radial_polynomial(m, n, r) == 6*np.power(r, 6) - 5*np.power(r, 4), f'Implemented R{m, n} not equal to tabulated Zernike'
+    assert radial_polynomial_derivative_dr(m, n, r) == 36*np.power(r, 5) - 20*r*r*r, f'Implemented dR{m, n} != to the derivative'
     print("All tests passed")
 
 
@@ -420,5 +423,7 @@ if __name__ == '__main__':
     plot_zps_polar(orders, step_r, step_theta, "Oblique astigmatism")
     orders = [(0, 2)]  # Y tilt
     plot_zps_polar(orders, step_r, step_theta, "Defocus")
-    orders = [(-1, 1), (1, 1)]; coefficients = [1.0, 1.0]  # Tilts
-    plot_zps_polar(orders, step_r, step_theta, "Sum tilts", tuned=True, alpha_coefficients=coefficients)
+    zernikes_set = [(-1, 1), (1, 1)]; coefficients = [1.0, 1.0]  # Tilts
+    plot_zps_polar(zernikes_set, step_r, step_theta, "Sum tilts", tuned=True, alpha_coefficients=coefficients)
+    zernikes_set = [(-2, 2), (0, 2), (2, 2)]; coefficients = [1.0, 1.0, 1.0]  # 2nd orders
+    plot_zps_polar(zernikes_set, step_r, step_theta, "Sum tilts", tuned=True, alpha_coefficients=coefficients)