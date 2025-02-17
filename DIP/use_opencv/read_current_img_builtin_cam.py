0571dd55aeddb1722535e1dd990391d211010d02
Sergei <sergej.klykow@gmail.com>
2025-02-17 13:00:05 +0100

    More methods tested for embedded camera controlled by opencv

--- DIP/use_opencv/read_current_img_builtin_cam.py
+++ DIP/use_opencv/read_current_img_builtin_cam.py
@@ -9,6 +9,7 @@ Attempt to access the built-in camera in the laptop.
 # %% Global Imports
 import matplotlib.pyplot as plt
 import cv2
+import traceback
 
 # %% References
 # 1) https://docs.opencv.org/4.10.0/dd/d43/tutorial_py_video_display.html
@@ -18,6 +19,7 @@ import cv2
 if __name__ == "__main__":
     # Loop for finding the available index of camera if any is available
     camera_handle = None; plt.close('all')
+    test_exp_time = False  # flag for testing below
 
     # Iteration over some range of indices for searching of an available camera
     for i in range(0, 6, 1):
@@ -40,7 +42,19 @@ if __name__ == "__main__":
         if camera.isOpened():
             print(f"Camera with index {i} is available and will be used")
             # camera.set(cv2.CAP_PROP_CONVERT_RGB, 1)  # not working directly
-            camera.set(cv2.CAP_PROP_GAIN, 0.75)
+            # Getting exposure time
+            current_exposure = camera.get(cv2.CAP_PROP_EXPOSURE)
+            print(f"Current Exposure Time: {current_exposure}")  # if negative value provided, then exposure time is auto selected
+            w = camera.get(cv2.CAP_PROP_FRAME_WIDTH); h = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            print("Camera Image WxH:", (w, h))
+            # Try to set custom exposure time
+            if test_exp_time:
+                try:
+                    current_exposure = camera.set(cv2.CAP_PROP_EXPOSURE, 2000)  # trying to set exposure time
+                    current_exposure = camera.get(cv2.CAP_PROP_EXPOSURE)  # getting back the exposure time (acknowledged by a camera)
+                    print(f"Set Exposure Time: {current_exposure}")
+                except Exception:
+                    traceback.print_exc()
             camera_handle = camera; break
         else:
             print(f"Camera with index {i} is not available")
@@ -48,7 +62,7 @@ if __name__ == "__main__":
     # Read a few images and show them using pyplot
     if camera_handle is not None and camera_handle.isOpened():
         for i in range(4):
-            camera.set(cv2.CAP_PROP_GAIN, 0.25*(i+1))
+            camera.set(cv2.CAP_PROP_GAIN, 0.5*(i+1))
             read_flag, frame = camera_handle.read()  # read single frame
             if read_flag:
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # required conversion from BGR to RGB
